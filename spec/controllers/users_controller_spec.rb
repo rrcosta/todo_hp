f63e387820f6545737ffc206ff460ef0b5194a2c
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-04-12 00:20:46 -0300

    Adds user controller test structure

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,117 @@
+require 'rails_helper'
+
+# Also compared to earlier versions of this generator, there are no longer any
+# expectations of assigns and templates rendered. These features have been
+# removed from Rails core in Rails 5, but can be added back in via the
+# `rails-controller-testing` gem.
+
+RSpec.describe UsersController, type: :controller do
+
+  let(:valid_attributes) {
+    skip("Add a hash of attributes valid for your model")
+  }
+
+  let(:invalid_attributes) {
+    skip("Add a hash of attributes invalid for your model")
+  }
+
+  let(:valid_session) { {} }
+
+  describe "GET #index" do
+    xit "returns a success response" do
+      User.create! valid_attributes
+      get :index, params: {}, session: valid_session
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #show" do
+    xit "returns a success response" do
+      user = User.create! valid_attributes
+      get :show, params: {id: user.to_param}, session: valid_session
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #new" do
+    xit "returns a success response" do
+      get :new, params: {}, session: valid_session
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #edit" do
+    xit "returns a success response" do
+      user = User.create! valid_attributes
+      get :edit, params: {id: user.to_param}, session: valid_session
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      xit "creates a new User" do
+        expect {
+          post :create, params: {user: valid_attributes}, session: valid_session
+        }.to change(User, :count).by(1)
+      end
+
+      xit "redirects to the created user" do
+        post :create, params: {user: valid_attributes}, session: valid_session
+        expect(response).to redirect_to(User.last)
+      end
+    end
+
+    context "with invalid params" do
+      xit "returns a success response (i.e. to display the 'new' template)" do
+        post :create, params: {user: invalid_attributes}, session: valid_session
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "with valid params" do
+      let(:new_attributes) {
+        skip("Add a hash of attributes valid for your model")
+      }
+
+      xit "updates the requested user" do
+        user = User.create! valid_attributes
+        put :update, params: {id: user.to_param, user: new_attributes}, session: valid_session
+        user.reload
+        skip("Add assertions for updated state")
+      end
+
+      xit "redirects to the user" do
+        user = User.create! valid_attributes
+        put :update, params: {id: user.to_param, user: valid_attributes}, session: valid_session
+        expect(response).to redirect_to(user)
+      end
+    end
+
+    context "with invalid params" do
+      xit "returns a success response (i.e. to display the 'edit' template)" do
+        user = User.create! valid_attributes
+        put :update, params: {id: user.to_param, user: invalid_attributes}, session: valid_session
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    xit "destroys the requested user" do
+      user = User.create! valid_attributes
+      expect {
+        delete :destroy, params: {id: user.to_param}, session: valid_session
+      }.to change(User, :count).by(-1)
+    end
+
+    xit "redirects to the users list" do
+      user = User.create! valid_attributes
+      delete :destroy, params: {id: user.to_param}, session: valid_session
+      expect(response).to redirect_to(users_url)
+    end
+  end
+
+end
