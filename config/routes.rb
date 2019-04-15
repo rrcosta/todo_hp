176fe35f3b79e42717b7c5c790bef5498113c024
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-04-15 11:23:50 -0300

    Adds new routes for Pending and completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'todo_lists#index'
+  root to: "todo_lists#index"
 
   resources :todo_lists
   resources :users, except: :new
+
+  get "pending", to: "todo_lists#pending"
+  get "completed", to: "todo_lists#completed"
 end
