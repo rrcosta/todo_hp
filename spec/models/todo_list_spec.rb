9fd42a1e4049c39ffa55015cd7717c2d173c18a2
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-04-15 01:22:40 -0300

    Creates scopes of TodoList

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -15,4 +15,36 @@ RSpec.describe TodoList, type: :model do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:description) }
   end
+
+  context "scopes" do
+    describe ".pending" do
+      let!(:todo_pending){ create(:todo_list, done: false) }
+
+      it 'returns all todolist done pending' do
+        expect(
+          described_class.all.pending.size
+        ).to eql(1)
+      end
+    end
+
+    describe ".completed" do
+      let!(:todo_completed){ create(:todo_list, done: true) }
+
+      it 'returns all todolist done completed' do
+        expect(
+          described_class.all.completed.size
+        ).to eql(1)
+      end
+    end
+
+    describe ".group_by_status" do
+      let!(:todo_true) { create(:todo_list, done: true) }
+
+      it 'returns the number of status/done' do
+        expect(
+          described_class.group_by_status
+        ).to eql({true => 1})
+      end
+    end
+  end
 end
