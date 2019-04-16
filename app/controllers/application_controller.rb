22057a519c38902dea9fde91bba33cfe546a9aad
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-04-16 14:40:49 -0300

    improves syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,12 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) do
-        |u| u.permit(:name, :email, :password)
+      devise_parameter_sanitizer.permit(:sign_up) do |u|
+        u.permit(:name, :email, :password)
       end
 
-      devise_parameter_sanitizer.permit(:account_update) do
-        |u| u.permit(:name, :email, :password, :current_password)
+      devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:name, :email, :password, :current_password)
       end
     end
 end
